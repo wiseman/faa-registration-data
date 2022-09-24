5fc65eee5f9e2515970c700aa2587132dff3fa22
John Wiseman <jjwiseman@gmail.com>
2022-09-24 16:01:23 -0700

    Ugh. Use deno to run python.

---
+++ postprocess.ts
@@ -0,0 +1,16 @@
+
+// install requirements with pip
+// const pip_install = Deno.run({
+//     cmd: ['python', '-m', 'pip', 'install', '-r', 'requirements.txt'],
+// });
+
+// await pip_install.status();
+
+
+
+// Forwards the execution to the python script
+const py_run = Deno.run({
+    cmd: ['python', './postprocess.py'].concat(Deno.args),
+});
+
+await py_run.status();
